07b9f21f7d238ae3bdaae4d23951edacb3afac25
Wilton Loiola <wloiola173@gmail.com>
2025-03-03 11:46:07 -0300

    commit settings DEBUG

--- projetointegrador/settings.py
+++ projetointegrador/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+#DEBUG = 'RENDER' not in os.environ
 
 #APPEND_SLASH = False
 ALLOWED_HOSTS = ['django-auth-crud-huzk.onrender.com']
